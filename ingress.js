daac7fa2380d9b7f3ff5884b3e5202562de47822
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2022-10-27 20:56:19 +0100

    removed old worker code

--- ingress.js
+++ ingress.js
@@ -1,142 +1,144 @@
-import dgram from "node:dgram";
+// depreciated 27 Oct 2022
 
-import config from "./config.js";
-import { Worker, isMainThread, parentPort } from "node:worker_threads";
-
-// --------------------creating a udp ingress --------------------
-
-// const newWorker = new Worker("./worker.js");
-// here create workers
-// the workers will be responsible for locating the file, converting it to binary
-const workerPool = [
-  // Start a pool of four workers
-  new Worker("./worker.js"),
-  new Worker("./worker.js"),
-  // new Worker("./worker.js"),
-  // new Worker("./worker.js"),
-];
-const waiting = [];
-
-// try creating worker
-// let myWorker = new MyWorker();
-
-// creating a udp ingress
-const ingress = dgram.createSocket("udp4");
-
-// emits when any error occurs
-ingress.on("error", (error) => {
-  console.log("udp_server", "error", error);
-  ingress.close();
-});
-
-// emits on new datagram msg
-ingress.on("message", (msg, info) => {
-  const genMsg = msg.toString().toLowerCase();
-  // check if msg is requesting a txt (make all case insensitive)
-  if (genMsg.includes("txt") || genMsg.includes("jpg")) {
-    console.log("has txt " + genMsg);
-    if (workerPool.length > 0) {
-      const newWorker = workerPool.shift();
-      handleRequest(newWorker, genMsg, info);
-    } else {
-      console.log("workerPool full for req " + genMsg);
-      waiting.push({ genMsg, info });
-    }
-  } else {
-    console.log("pls specify txt");
-    // define response
-    let timestp = new Date();
-    let response = {
-      description: "please specify a txt file you're looking for",
-      status: 400,
-      contentReturned: null,
-      serverPort: config.port,
-      timestamp: timestp.toJSON(),
-      worker: null,
-      received: {
-        message: msg.toString(),
-        fromIP: info.address,
-        fromPort: info.port,
-      },
-    };
-    // convert resp to buffer
-    const data = Buffer.from(JSON.stringify(response));
-    //sending msg
-    ingress.send(data, info.port, info.address, (error, bytes) => {
-      if (error) {
-        console.log("udp_server", "error", error);
-        ingress.close();
-      } else {
-        console.log("udp_server", "info", "Data sent");
-      }
-    });
-  }
-}); // end ingress.on
-
-function handleRequest(worker, msg, info) {
-  console.log("processing " + msg);
-  // send the req to the worker so it can get the file
-  worker.postMessage(msg);
-  // define response
-  let timestp = new Date();
-  let response = {
-    description: "UDP PORT TEST BY Diarmuid McGonagle",
-    status: 0,
-    contentReturned: null,
-    serverPort: config.port,
-    timestamp: timestp.toJSON(),
-    worker: null,
-    received: {
-      message: msg.toString(),
-      fromIP: info.address,
-      fromPort: info.port,
-    },
-  };
-  // when it gets the file, itll return here
-  worker.once("message", (fileInfo) => {
-    response.description = fileInfo.description;
-    response.status = fileInfo.status;
-    // set content here (will prolly need to change)
-    response.contentReturned = fileInfo.message;
-    // convert resp to buffer
-    const data = Buffer.from(JSON.stringify(response));
-    //sending msg
-    ingress.send(data, info.port, info.address, (error, bytes) => {
-      if (error) {
-        console.log("udp_server", "error", error);
-        ingress.close();
-      } else {
-        console.log("udp_server", "info", "Data sent w msg " + msg);
-        // If requests are waiting, reuse the current worker to handle the queued
-        // request. Add the worker to pool if no requests are queued.
-        if (waiting.length > 0) {
-          const newJob = waiting.shift();
-          console.log("waiting large " + msg);
-          handleRequest(worker, newJob.genMsg, newJob.info);
-        } else {
-          console.log("waiting 0 so putting worker back");
-          workerPool.push(worker);
-        }
-      }
-    });
-  });
-}
-
-//emits when socket is ready and listening for datagram msgs
-ingress.on("listening", () => {
-  const address = ingress.address();
-  const port = address.port;
-  const family = address.family;
-  const ipaddr = address.address;
-
-  console.log("udp_server", "info", "ingress is listening at port " + port);
-  console.log("udp_server", "info", "ingress ip :" + ipaddr);
-  console.log("udp_server", "info", "ingress is IP4/IP6 : " + family);
-});
-
-//emits after the socket is closed using socket.close()
-ingress.on("close", () => {
-  console.log("udp_server", "info", "Socket is closed !");
-});
-
-ingress.bind(config.port);
+// import dgram from "node:dgram";
+//
+// import config from "./config.js";
+// import { Worker, isMainThread, parentPort } from "node:worker_threads";
+//
+// // --------------------creating a udp ingress --------------------
+//
+// // const newWorker = new Worker("./worker.js");
+// // here create workers
+// // the workers will be responsible for locating the file, converting it to binary
+// const workerPool = [
+//   // Start a pool of four workers
+//   new Worker("./worker.js"),
+//   new Worker("./worker.js"),
+//   // new Worker("./worker.js"),
+//   // new Worker("./worker.js"),
+// ];
+// const waiting = [];
+//
+// // try creating worker
+// // let myWorker = new MyWorker();
+//
+// // creating a udp ingress
+// const ingress = dgram.createSocket("udp4");
+//
+// // emits when any error occurs
+// ingress.on("error", (error) => {
+//   console.log("udp_server", "error", error);
+//   ingress.close();
+// });
+//
+// // emits on new datagram msg
+// ingress.on("message", (msg, info) => {
+//   const genMsg = msg.toString().toLowerCase();
+//   // check if msg is requesting a txt (make all case insensitive)
+//   if (genMsg.includes("txt") || genMsg.includes("jpg")) {
+//     console.log("has txt " + genMsg);
+//     if (workerPool.length > 0) {
+//       const newWorker = workerPool.shift();
+//       handleRequest(newWorker, genMsg, info);
+//     } else {
+//       console.log("workerPool full for req " + genMsg);
+//       waiting.push({ genMsg, info });
+//     }
+//   } else {
+//     console.log("pls specify txt");
+//     // define response
+//     let timestp = new Date();
+//     let response = {
+//       description: "please specify a txt file you're looking for",
+//       status: 400,
+//       contentReturned: null,
+//       serverPort: config.port,
+//       timestamp: timestp.toJSON(),
+//       worker: null,
+//       received: {
+//         message: msg.toString(),
+//         fromIP: info.address,
+//         fromPort: info.port,
+//       },
+//     };
+//     // convert resp to buffer
+//     const data = Buffer.from(JSON.stringify(response));
+//     //sending msg
+//     ingress.send(data, info.port, info.address, (error, bytes) => {
+//       if (error) {
+//         console.log("udp_server", "error", error);
+//         ingress.close();
+//       } else {
+//         console.log("udp_server", "info", "Data sent");
+//       }
+//     });
+//   }
+// }); // end ingress.on
+//
+// function handleRequest(worker, msg, info) {
+//   console.log("processing " + msg);
+//   // send the req to the worker so it can get the file
+//   worker.postMessage(msg);
+//   // define response
+//   let timestp = new Date();
+//   let response = {
+//     description: "UDP PORT TEST BY Diarmuid McGonagle",
+//     status: 0,
+//     contentReturned: null,
+//     serverPort: config.port,
+//     timestamp: timestp.toJSON(),
+//     worker: null,
+//     received: {
+//       message: msg.toString(),
+//       fromIP: info.address,
+//       fromPort: info.port,
+//     },
+//   };
+//   // when it gets the file, itll return here
+//   worker.once("message", (fileInfo) => {
+//     response.description = fileInfo.description;
+//     response.status = fileInfo.status;
+//     // set content here (will prolly need to change)
+//     response.contentReturned = fileInfo.message;
+//     // convert resp to buffer
+//     const data = Buffer.from(JSON.stringify(response));
+//     //sending msg
+//     ingress.send(data, info.port, info.address, (error, bytes) => {
+//       if (error) {
+//         console.log("udp_server", "error", error);
+//         ingress.close();
+//       } else {
+//         console.log("udp_server", "info", "Data sent w msg " + msg);
+//         // If requests are waiting, reuse the current worker to handle the queued
+//         // request. Add the worker to pool if no requests are queued.
+//         if (waiting.length > 0) {
+//           const newJob = waiting.shift();
+//           console.log("waiting large " + msg);
+//           handleRequest(worker, newJob.genMsg, newJob.info);
+//         } else {
+//           console.log("waiting 0 so putting worker back");
+//           workerPool.push(worker);
+//         }
+//       }
+//     });
+//   });
+// }
+//
+// //emits when socket is ready and listening for datagram msgs
+// ingress.on("listening", () => {
+//   const address = ingress.address();
+//   const port = address.port;
+//   const family = address.family;
+//   const ipaddr = address.address;
+//
+//   console.log("udp_server", "info", "ingress is listening at port " + port);
+//   console.log("udp_server", "info", "ingress ip :" + ipaddr);
+//   console.log("udp_server", "info", "ingress is IP4/IP6 : " + family);
+// });
+//
+// //emits after the socket is closed using socket.close()
+// ingress.on("close", () => {
+//   console.log("udp_server", "info", "Socket is closed !");
+// });
+//
+// ingress.bind(config.port);
--- worker.js
+++ worker.js
@@ -1,38 +1,165 @@
-import { parentPort } from "node:worker_threads";
+import dgram from "node:dgram";
+
+import conf from "./config.js";
+import * as readline from "readline";
 
 import fs from "fs";
 var files = fs.readdirSync("./filesToReturn");
 
-parentPort.on("message", (textData) => {
-  const solution = identifyFile(textData);
-  parentPort.postMessage(solution);
+console.log("creating worker");
+// creating a worker socket
+const worker = dgram.createSocket("udp4");
+
+// Setup readline functionalities
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-// checks textData for a file & then either begins
-// processing it or returns that it doesnt exist
-function identifyFile(stringData) {
-  if (files.includes(stringData)) {
-    return convertFileToBinary(stringData);
-  } else
-    return {
-      status: "404",
-      description: "File Not Found",
-      message:
-        "could not locate file " + stringData + " in " + JSON.stringify(files),
-    };
-  // now check if this name is included in list of files
+// var for how many file reqs sent at once, used to prompt user input again
+let fileReturned;
+
+function readLineAsync(message) {
+  return new Promise((resolve, reject) => {
+    rl.question(message, (answer) => {
+      const underCaseAnswer = answer.toLowerCase();
+
+      // exit process if exit
+      if (underCaseAnswer == "exit") sendCloseDownMessage();
+      else {
+        if (underCaseAnswer.includes("1") || underCaseAnswer.includes("refunk"))
+          fileReturned = 0;
+        else if (
+          underCaseAnswer.includes("2") ||
+          underCaseAnswer.includes("weeve")
+        )
+          fileReturned = 1;
+        else if (
+          underCaseAnswer.includes("3") ||
+          underCaseAnswer.includes("pic")
+        )
+          fileReturned = 2;
+        else {
+          console.log("That is not valid input. try again\n");
+          handleServerInput(
+            "What file will this worker forward?\n1. refunk\n2. weeve\n3. picture\n\n"
+          );
+        }
+        sendSetUpMessage(fileReturned);
+        handleServerInput("\ntype 'exit' to quit\n");
+      }
+      // send init msg
+      resolve(answer);
+    });
+  });
 }
 
-// if needed, may need to convert txt/img files into binary so that
-// they can be sent to the client. this function will do that
-function convertFileToBinary(stringData) {
-  const contents = fs.readFileSync(`./filesToReturn/${stringData}`, {
+async function handleServerInput(msg) {
+  await readLineAsync(msg);
+}
+
+// initial ask for user input
+handleServerInput(
+  "What file will this worker forward?\n1. refunk\n2. weeve\n3. picture\n\n"
+);
+
+worker.on("message", (msg, info) => {
+  console.log("Data received from server : " + msg.toString());
+  let particularFile;
+  switch (fileReturned) {
+    case 0:
+      particularFile = "refunk.txt";
+      break;
+    case 1:
+      particularFile = "weeve.txt";
+      break;
+    case 2:
+      particularFile = "basic_pic.jpg";
+      break;
+    default:
+  }
+
+  const contents = fs.readFileSync(`./filesToReturn/${particularFile}`, {
     encoding: "base64",
   });
 
-  return {
-    status: "200",
-    description: "File Found",
-    message: contents,
-  };
+  sendFileMessage(msg, contents);
+});
+
+function sendSetUpMessage(fileToReturn) {
+  // create header
+  const header = new Uint8Array(2);
+  // since worker setup, first header byte is 1
+  header[0] = 1;
+  // set second headerbyte to file to be returned
+  header[1] = fileToReturn;
+  const data = Buffer.from(header);
+
+  //sending msg
+  worker.send(data, conf.port, conf.serverHost, (error) => {
+    if (error) {
+      console.log(error);
+      worker.close();
+    } else {
+      console.log(
+        "single msg sent to ingress from ",
+        conf.serverHost,
+        conf.port
+      );
+    }
+  });
+}
+
+function sendFileMessage(msg, file) {
+  console.log("sending file ");
+  console.log("client id is " + msg[2]);
+  // create header
+  const header = new Uint8Array(3);
+  // since worker returning file, first header byte is 3
+  header[0] = 3;
+  // set second headerbyte to file to be returned
+  header[1] = fileReturned;
+  header[2] = msg[2];
+  const data = Buffer.from(header);
+
+  //sending msg
+  worker.send([data, file], conf.port, conf.serverHost, (error) => {
+    if (error) {
+      console.log(error);
+      worker.close();
+    } else {
+      console.log(
+        "single msg sent to ingress from ",
+        conf.serverHost,
+        conf.port
+      );
+    }
+  });
+}
+
+function sendCloseDownMessage(fileToReturn) {
+  // create header
+  const header = new Uint8Array(2);
+  // since worker close down, first header byte is 5
+  header[0] = 5;
+  // set second headerbyte to file to be returned
+  header[1] = fileToReturn;
+  const data = Buffer.from(header);
+  console.log("sending close down client");
+
+  //sending msg
+  worker.send(data, conf.port, conf.serverHost, (error) => {
+    if (error) {
+      console.log(error);
+      worker.close();
+    } else {
+      console.log(
+        "single msg sent to ingress from ",
+        conf.serverHost,
+        conf.port
+      );
+      worker.close();
+      process.exit();
+    }
+  });
 }
